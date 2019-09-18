5075e6b507b7eefc5d19cf3c67e4a75d765c4159
Harish BS <bs.harish@gmail.com>
2019-09-18 11:58:19 +0530

    Updated client side js

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener("submit", e => {
   e.preventDefault();
   const loc = searchText.value;
 
-  fetch("http://localhost:3000/weather?address=" + loc).then(response => {
+  fetch("/weather?address=" + loc).then(response => {
     response.json().then(data => {
       if (data.error) {
         console.log(error);
